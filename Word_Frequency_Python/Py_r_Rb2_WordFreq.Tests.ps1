3264857c3e3ef98ea7fa6eba4083568e3ddb5aa8
prsshini <prsshini@gmail.com>
2018-03-06 02:16:10 -0800

    Adding powershell scripts for word freq problem

---
+++ Word_Frequency_Python/Py_r_Rb2_WordFreq.Tests.ps1
@@ -0,0 +1,28 @@
+﻿$here = Split-Path -Parent $MyInvocation.MyCommand.Path
+$sut = (Split-Path -Leaf $MyInvocation.MyCommand.Path) -replace '\.Tests\.', '.'
+. "$here\$sut"
+
+Describe "Test_wrong_input_param" {
+    it  'only allows a string to be passed to the FilePath parameter_when called' {
+
+  mock 'Select-String'  {
+
+  return $null
+
+ }
+{find_word_freq -FileName ([PSCustomObject]@{})} | Should throw
+}
+}
+
+
+describe 'Test file 2' {
+
+    $FileName = "C:\Users\Michael\Desktop\Teradici\Teradici_Tech_Assnmt\Wordfreqinput2.txt"
+    $result = find_word_freq -FileName 'C:\Users\Michael\Desktop\Teradici\Teradici_Tech_Assnmt\Wordfreqinput2.txt'
+
+    it 'should return 7 ones' {
+        {find_word_freq -FileName $FileName} | Should not throw
+    }
+
+}
+
---
+++ Word_Frequency_Python/Py_r_Rb2_WordFreq.ps1
@@ -0,0 +1,50 @@
+﻿param (
+    [Parameter(Mandatory=$true)][string]$FileName                
+ )
+
+Function find_word_freq {
+[
+CmdletBinding()]
+
+  param
+
+  (
+
+  [string]$FileName
+
+  )
+##$FileName = "C:\Users\Michael\Desktop\Teradici\Teradici_Tech_Assnmt\Wordfreqinput.txt"
+$File = Get-Content $FileName
+$Linecount = $File.Count
+Write-Host "Total no of lines in the file $FileName : $Linecount"
+
+$words = ""
+$exist = 0
+$Count = 0
+$wordarr = @{}
+$linect = 0
+
+$File | foreach {
+    $Line = $_
+    $linect++ 
+    $Line.Split(" .,:;?!/()[]{}-```"$*@#%^&~") | foreach {
+        $word = $_.ToUpper()
+         If ($word[0] -replace '[^a-zA-Z0-9]', '') {
+            $Count++
+            If ($word.Contains($words)) { $exist++ }
+            If ($wordarr.ContainsKey($Word)) {
+                $wordarr.$word++
+            } else {
+                $wordarr.Add($word, 1)
+            }
+        }
+    } 
+}
+
+$allwords = $wordarr.Count
+Write-Host "There were $allwords total words in the text"
+$wordarr.GetEnumerator() | ? { $_.Name.Length -gt 1 } | 
+Sort Value -Descending | Select -First 10
+}
+
+find_word_freq -FileName $FileName
