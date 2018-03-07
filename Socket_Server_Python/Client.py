0c7e2b45eeeed6a19c88b50c26ad3760b097f103
prsshini <prsshini@gmail.com>
2018-03-07 09:18:54 -0800

    updating Client Program - (Server Program was checked into client by mistake last time)

--- Socket_Server_Python/Client.py
+++ Socket_Server_Python/Client.py
@@ -1,36 +1,18 @@
-# Echo server program
+# Echo client program
 import socket
 
-HOST = 'localhost'
-PORT = 50007
+HOST = 'localhost'  # The remote host
 
-#function to reverse the string
-def reverse_string(data):
-    response=data[::-1]
-    return ( response )
+PORT = 50007        # The same port as used by the server
 
-def mysocket():
-    so = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+so = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-#Bind the socket to the host and port
-    so.bind((HOST, PORT))
+so.connect((HOST, PORT))
 
-#Start to listen
-    so.listen(1)
+so.send(' Box')
 
-#Connect to the host
-    conn, addr = so.accept()
-    print 'Connected to', addr
-    while 1:
-        data = conn.recv(1024)
-        print ' Server received the data  : ', data
-        if not data:
-            break
-        response=reverse_string(data)
-        print 'String received is reversed: ', response
-        conn.send(response)
-    conn.close()
+data = so.recv(1024)
 
+so.close()
 
-if __name__ == '__main__':
-    mysocket()
+print 'Response Received in the clinet : ', repr(data)
